589ec1db9031379515089099d472fc4b0a0e63b1
Vitor Henrique <vhenriquebm@gmail.com>
2022-01-13 15:10:42 -0300

    Adicionado campo com vidas, lógica que faz o mosquito aparecer em vários pontos da tela, lógica que faz imagem aparecer e vida não acabar quando você clica no mosquito e lógica que muda para tela game over e reinicia

--- jogo.js
+++ jogo.js
@@ -1,35 +1,84 @@
+
 var altura = 0
 var largura = 0
+var vidas = 1
 
-function ajustaTamanhoPalcoJogo () {
-altura = window.innerHeight
-largura = window.innerWidth
+function ajustaTamanhoPalcoJogo() {
+	altura = window.innerHeight
+	largura = window.innerWidth
 
-console.log (largura, altura)
+	console.log(largura, altura)
 }
 
-
-ajustaTamanhoPalcoJogo ()
+ajustaTamanhoPalcoJogo()
 
 
 function posicaoRandomica() {
 
-var posicaoX = Math.floor(Math.random() * largura) - 90
-var posicaoY = Math.floor(Math.random() * altura) - 90
 
-posicaoX = posicaoX < 0 ? 0 : posicaoX
-posicaoY = posicaoX < 0 ? 0 : posicaoY
+	//remover o mosquito anterior (caso exista)
+	if(document.getElementById('mosquito')) {
+		document.getElementById('mosquito').remove()
 
+		//console.log('elemento selecionado foi: v' + vidas)
+		if(vidas > 3) {
 
-console.log(posicaoX, posicaoY)
+			window.location.href = 'fim_de_jogo.html'
+		} else {
+			document.getElementById('v' + vidas).src = "imagens/coracao_vazio.png"
 
-var mosquito = document.createElement('img')
-mosquito.src = 'imagens/mosca.png'
-mosquito.className = 'mosquito1'
-mosquito.style.left = posicaoX + 'px'
-mosquito.style.top = posicaoY + 'px'
-mosquito.style.position = 'absolute'
+			vidas++
+		}
+	}
 
-document.body.appendChild(mosquito)
+	var posicaoX = Math.floor(Math.random() * largura) - 90
+	var posicaoY = Math.floor(Math.random() * altura) - 90
 
+	posicaoX = posicaoX < 0 ? 0 : posicaoX
+	posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+	console.log(posicaoX, posicaoY)
+
+	//criar o elemento html
+	var mosquito = document.createElement('img')
+	mosquito.src = 'imagens/mosca.png'
+	mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
+	mosquito.style.left = posicaoX + 'px'
+	mosquito.style.top = posicaoY + 'px'
+	mosquito.style.position = 'absolute'
+	mosquito.id = 'mosquito'
+	mosquito.onclick = function() {
+		this.remove()
+	}
+
+	document.body.appendChild(mosquito)
+
+}
+
+function tamanhoAleatorio() {
+	var classe = Math.floor(Math.random() * 3)
+	
+	switch(classe) {
+		case 0:
+			return 'mosquito1'
+		
+		case 1:
+			return 'mosquito2'
+
+		case 2:
+			return 'mosquito3'
+	}
+}
+
+function ladoAleatorio() {
+	var classe = Math.floor(Math.random() * 2)
+	
+	switch(classe) {
+		case 0:
+			return 'ladoA'
+		
+		case 1:
+			return 'ladoB'
+
+	}
 }
